f4b9ec32884ed695c23afa6f4fa432128f3fad35
Ahad Hosen Akash <162029494+AHAAkash@users.noreply.github.com>
2025-01-27 01:17:39 +0600

    Add files via upload

---
+++ Case_converstion.asm
@@ -0,0 +1,39 @@
+
+TITLE PGM4_3: CASE CONVERSTION PROGRAM
+.MODEL SMALL 
+.STACK 100H  
+.DATA  
+  CR EQU 0DH
+  LE EQU 0AH
+MSG1 DB 'ENTER A LOWER CASE LETTER: $'   
+MSG2 DB 0DH ,0AH, 'IN UPPER CASE IT IS: '
+CHAR DB ?,'$'
+.CODE 
+MAIN PROC  
+      
+    ;initialize DS   
+    MOV AX, @DATA     ; get data segment 
+    MOV DS, AX        ; initialize DS  
+    
+    ;print user prompt 
+    LEA DX, MSG1      ; get first message
+    MOV AH, 9         ; display string function 
+    INT 21H           ; display first message
+    
+    ;input a character and convert to upper case
+    MOV AH, 1         ; read character function
+    INT 21H           ; read a small letter into AL
+    SUB AL, 20H       ; convert it to upper case
+    MOV CHAR, AL      ; and store i
+                        
+    ;display on the next line 
+    LEA DX, MSG2      ; get second message
+    MOV AH, 9         ; display string function
+    INT 21H           ; display message and upper case
+    
+    ;DOS exit         ; letter in front
+    MOV AH, 4CH  
+    INT 21H           ;DOS exit
+  
+MAIN ENDP 
+END MAIN
